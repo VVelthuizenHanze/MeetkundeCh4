c185ca9fac660005e8834fe7ac7a5fa7d064f6a4
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2020-09-29 09:55:04 +0200

    Interfaces (dagdeel 7) je moet wel de goede figuren gebruiken...

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -16,15 +16,13 @@ public class MeetkundeLauncher {
     public static void main(String[] args) {
 
         Oppervlak oppervlak = new Oppervlak(10, 7);
-
-        Oppervlak oppervlak2 = new Oppervlak(10, 7);
-        oppervlak2.voegFiguurToe(new Rechthoek(4, 3, new Punt(0, 7), "rood"));
-        oppervlak2.voegFiguurToe(new Cirkel(4, new Punt(1.5, 1.5), "geel"));
-        oppervlak2.voegFiguurToe(new Rechthoek(9, 8, new Punt(3, 2), "groen"));
-        oppervlak2.voegFiguurToe(new Rechthoek(4, 1, new Punt(5, 7), "oranje"));
-        oppervlak2.voegFiguurToe(new Cirkel(2, new Punt(8, 5), "paars"));
-        oppervlak2.voegFiguurToe(new Rechthoek(11, 3, new Punt(5, 3), "blauw"));
-        System.out.println(oppervlak2.toString());
+        oppervlak.voegFiguurToe(new Rechthoek(3, 3, new Punt(0, 7), "rood"));
+        oppervlak.voegFiguurToe(new Rechthoek(3, 2, new Punt(0, 4), "geel"));
+        oppervlak.voegFiguurToe(new Rechthoek(5, 2, new Punt(0, 2), "groen"));
+        oppervlak.voegFiguurToe(new Rechthoek(5, 2, new Punt(3, 7), "paars"));
+        oppervlak.voegFiguurToe(new Rechthoek(5, 4, new Punt(5, 7), "oranje"));
+        oppervlak.voegFiguurToe(new Rechthoek(5, 3, new Punt(5, 3), "blauw"));
+        System.out.println(oppervlak);
 
     }
 
