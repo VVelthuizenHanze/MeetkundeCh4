eb92689ff0c3ff04cdd204c4961a11a25cd9b1f3
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2020-10-05 09:59:48 +0200

    Dagdeel 9 deel 1 (lezen uit bestand

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -2,9 +2,11 @@ package controller;
 
 import model.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
+
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -17,31 +19,41 @@ public class MeetkundeLauncher {
 
     public static void main(String[] args) {
 
-        Scanner keyboard = new Scanner(System.in);
-        boolean onjuistInvoer = true;
+        ArrayList<String> regelsUitBestand = new ArrayList<>();
+        File rechthoekenBestand = new File("resources/Rechthoek.csv");
 
-        while(onjuistInvoer) {
-            System.out.print("Geef een straal: ");
-
-            try {
-                double straal = keyboard.nextDouble();
-                Cirkel ingevoerdeCirkel = new Cirkel(straal);
-                System.out.println(ingevoerdeCirkel);
-                onjuistInvoer = false;
-            } catch (IllegalArgumentException exception) {
-                // System.out.println("Dat was geen geldige straal");
-                System.out.println(exception.getMessage());
-            } catch (InputMismatchException exception) {
-                System.out.println("Dat is geen getal");
-                keyboard.nextLine();
-            } finally {
-                System.out.println("Je invoer is afgehandeld.");
+        Scanner invoer = null;
+        try {
+            invoer = new Scanner(rechthoekenBestand);
+            while (invoer.hasNextLine()) {
+                regelsUitBestand.add(invoer.nextLine());
+            }
+        } catch (FileNotFoundException exception) {
+            // exception.printStackTrace();
+            System.out.println("Het bestand is niet gevonden.");
+        } finally {
+            if (invoer != null) {
+                invoer.close();
             }
         }
+        
+        if (regelsUitBestand.size() > 0) {
+            ArrayList<Rechthoek> rechthoeken = new ArrayList<>();
+            for (String regel : regelsUitBestand) {
+                String[] regelArray = regel.split(",");
+                double lengte = Double.parseDouble(regelArray[0]);
+                double breedte = Double.parseDouble(regelArray[1]);
+                double xCoordinaat = Double.parseDouble(regelArray[2]);
+                double yCoordinaat = Double.parseDouble(regelArray[3]);
+                String kleur = regelArray[4];
+                rechthoeken.add(new Rechthoek(lengte, breedte, new Punt(xCoordinaat, yCoordinaat), kleur));
+            }
 
-
-
-
+            for (Rechthoek rechthoek : rechthoeken) {
+                System.out.println(rechthoek);
+                System.out.println();
+            }
+        }
     }
 
     public static void toonInformatie(Figuur figuur) {
